8310027c8057ee4c21ba6ae6d333c0e71eaa6870
michael lor and yvonna contreras <michael.vang.1985@gmail.com>
2016-03-22 16:12:19 -0700

    Add select into template, add onChange into Component

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -3,13 +3,20 @@ import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
 import { NewTaskComponent } from './new-task.component';
+import {DonePipe} from './done.pipe';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
   directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
+  pipes: [DonePipe],
   template: `
+  <select (change)="onChange($event.target.value)">
+    <option value="all">Show All</option>
+    <option value="done">Show Done</option>
+    <option value="notDone" selected="selected">Show Not Done</option>
+  </select>
   <task-display *ngFor="#currentTask of taskList"
     (click)="taskClicked(currentTask)"
     [class.selected]="currentTask === selectedTask"
@@ -24,6 +31,7 @@ export class TaskListComponent {
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
+  public filterDone: string = "notDone";
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
@@ -36,5 +44,9 @@ export class TaskListComponent {
     this.taskList.push(
       new Task(description, this.taskList.length)
     );
+  }
+  onChange(filterOption){
+    this.filterDone = filterOption;
+    console.log(this.filterDone);
   }
 }
