4ca442854731d089bc580a305aaea1c38708bee5
michael lor and yvonna contreras <michael.vang.1985@gmail.com>
2016-03-22 10:56:38 -0700

    Add taskWasSelected click feature

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,14 @@ import { Component } from 'angular2/core';
   template: `
     <div class="container">
       <h1>To-Do List</h1>
-      <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
+      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
+        {{ task.description }}
+      </h3>
     </div>
   `
 })
 export class AppComponent {
-  public tasks: Task[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  public tasks: Task[];
   constructor(){
     this.tasks = [
       new Task("Create To-Do List app.", 0),
@@ -18,6 +20,9 @@ export class AppComponent {
       new Task("Rewatch all the Lord of the Rings movies.", 2),
       new Task("Do the laundry.", 3)
     ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+  console.log(clickedTask);
   }
 }
 
